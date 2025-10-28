68215e12dcedd7c8a5c996936a41e7730f34036b
segefjord <ferryisworking@gmail.com>
2025-10-28 23:06:47 +0100

    🔨 crossOriginIsolated

--- middleware.js
+++ middleware.js
@@ -10,7 +10,7 @@ export default async function middleware(request) {
   // Clone the original request and forward it
   const res = await fetch(request);
 
-  console.log("MIDDLEWARE!", JSON.stringify(request?.nextUrl))
+  console.log("MIDDLEWARE!", JSON.stringify(request))
 
   // Clone the response so we can modify headers
   const newHeaders = new Headers(res.headers);
